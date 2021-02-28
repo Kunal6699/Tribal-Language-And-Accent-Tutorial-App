1dac1be0d88d513e16cbbc4d7451d0b82b25da9e
Kunal6699 <54436558+Kunal6699@users.noreply.github.com>
2021-02-28 11:12:02 +0530

    Final Commits

---
+++ app/src/main/java/com/example/miwok/Word.java
@@ -0,0 +1,75 @@
+package com.example.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    private int mAudioResourceId;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     * @param  imageResourceId is the resource id of image
+     */
+
+    public Word(String defaultTranslation, String miwokTranslation,int imageResourceId,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId=imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public String getDefaultTranslation()
+    {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation()
+    {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId()
+    {
+        return mImageResourceId;
+    }
+    public boolean hasImage()
+    {
+        return mImageResourceId !=NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId()
+    {
+        return mAudioResourceId;
+    }
+
+}
